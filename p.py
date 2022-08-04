467fd8d6c5332555e5f1e29d5b23b12f927190e2
Mohammad Mahboubian <mohammad.mahboubian@deliveryhero.com>
2022-08-04 14:29:25 +0200

    second

---
+++ p.py
@@ -0,0 +1,28 @@
+# Q1 What is the problem with this code?
+
+def foo(bar=[]):        
+    bar.append("baz")
+    return bar
+
+# Q2 What is the output of this code?
+x = 10
+def foo():
+    x += 1
+    print x
+
+
+# Q3 What is the output of this code?
+lst = [1, 2, 3]
+def foo1():
+    lst.append(5)
+    print(lst)
+
+foo1()
+
+# Q4 What is the output of this code?
+lst = [1, 2, 3]
+def foo2():
+    lst += [5]
+    print(lst)
+
+foo2()
