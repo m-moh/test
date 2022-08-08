65da075a52a36cd4b0609375e1cd1479525f6fc2
m-moh <98693820+m-moh@users.noreply.github.com>
2022-08-08 09:19:56 +0200

    Update p.py

--- p.py
+++ p.py
@@ -4,14 +4,18 @@ def foo(bar=[]):
     bar.append("baz")
     return bar
 
+
 # Q2 What is the output of this code?
+
 x = 10
 def foo():
     x += 1
     print x
 
 
+
 # Q3 What is the output of this code?
+
 lst = [1, 2, 3]
 def foo1():
     lst.append(5)
@@ -19,10 +23,23 @@ def foo1():
 
 foo1()
 
+
 # Q4 What is the output of this code?
+
 lst = [1, 2, 3]
 def foo2():
     lst += [5]
     print(lst)
 
 foo2()
+
+
+# Q5 What is the output of this code?
+
+odd = lambda x : bool(x % 2)
+numbers = [n for n in range(10)]
+for i in range(len(numbers)):
+    if odd(numbers[i]):
+        del numbers[i]
+        
+        
